909e6d9608624b9847aa3547825daf381bb6f5c0
JHelpGG <jhelpgg@gmail.com>
2023-05-28 15:29:29 +0200

    Empty component with start preferred size

--- Engine3D/src/main/kotlin/khelp/engine3d/gui/component/GUIComponentEmpty.kt
+++ Engine3D/src/main/kotlin/khelp/engine3d/gui/component/GUIComponentEmpty.kt
@@ -3,11 +3,15 @@ package khelp.engine3d.gui.component
 import khelp.engine3d.gui.GUIMargin
 import java.awt.Dimension
 import java.awt.Graphics2D
+import kotlin.math.max
 
-class GUIComponentEmpty : GUIComponent()
+class GUIComponentEmpty(preferredWidth : Int = 32, preferredHeight : Int = preferredWidth) : GUIComponent()
 {
+    private val preferredWidth : Int = max(16, preferredWidth)
+    private val preferredHeight : Int = max(16, preferredHeight)
+
     override fun drawIntern(graphics2D : Graphics2D, margin : GUIMargin) : Unit = Unit
 
     override fun preferredSize(margin : GUIMargin) : Dimension =
-        Dimension(margin.width + 16, margin.height + 16)
+        Dimension(margin.width + this.preferredWidth, margin.height + this.preferredHeight)
 }
