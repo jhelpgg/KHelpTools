b4791af2d3881a57bc704c249c31b06739a4c0c3
JHelpGG <jhelpgg@gmail.com>
2023-05-29 09:37:33 +0200

    Image constraint

--- Engine3D/src/main/kotlin/khelp/engine3d/gui/component/GUIComponentImage.kt
+++ Engine3D/src/main/kotlin/khelp/engine3d/gui/component/GUIComponentImage.kt
@@ -1,16 +1,52 @@
 package khelp.engine3d.gui.component
 
 import khelp.engine3d.gui.GUIMargin
-import khelp.ui.extensions.drawImage
 import khelp.ui.game.GameImage
 import java.awt.Dimension
 import java.awt.Graphics2D
+import kotlin.math.min
 
-class GUIComponentImage(var image : GameImage = GameImage.DUMMY) : GUIComponent()
+class GUIComponentImage(var image : GameImage = GameImage.DARK_LIGHT,
+                        var imageConstraint : ImageConstraint = ImageConstraint.FIT_PROPORTION) : GUIComponent()
 {
     override fun drawIntern(graphics2D : Graphics2D, margin : GUIMargin)
     {
-        graphics2D.drawImage(margin.left, margin.top, this.image)
+        val parentWidth = this.width - margin.width
+        val parentHeight = this.height - margin.height
+        var x = margin.left
+        var y = margin.top
+        var width = this.image.width
+        var height = this.image.height
+
+        when (this.imageConstraint)
+        {
+            ImageConstraint.CUT_CENTER     ->
+            {
+                x += (parentWidth - width) / 2
+                y += (parentHeight - height) / 2
+            }
+
+            ImageConstraint.FIT            ->
+            {
+                width = parentWidth
+                height = parentHeight
+            }
+
+            ImageConstraint.FIT_PROPORTION ->
+            {
+                val widthFactor = parentWidth.toDouble() / width.toDouble()
+                val heightFactor = parentHeight.toDouble() / height.toDouble()
+                val factor = min(widthFactor, heightFactor)
+
+                width = (width * factor).toInt()
+                height = (height * factor).toInt()
+
+                x += (parentWidth - width) / 2
+                y += (parentHeight - height) / 2
+            }
+        }
+
+        this.image.drawOn(graphics2D, x, y, width, height)
     }
 
     override fun preferredSize(margin : GUIMargin) : Dimension = Dimension(this.image.width + margin.width,
---
+++ Engine3D/src/main/kotlin/khelp/engine3d/gui/component/ImageConstraint.kt
@@ -0,0 +1,11 @@
+package khelp.engine3d.gui.component
+
+enum class ImageConstraint
+{
+    /** Cut image if image too big, center image if too small */
+    CUT_CENTER,
+    /** Fit to size */
+    FIT,
+    /** Fit and keep proportion */
+    FIT_PROPORTION
+}
